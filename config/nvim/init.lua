feeda5fab7f9f51cf6ed71be2360fdef9e5eedaf
Calvin Henderson <calvin_henderson@etownschools.org>
2023-06-23 13:43:26 -0400

    fixed nvim to auto-quit when only nvim-tree is left

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -696,6 +696,15 @@ if timer ~= nil then
 end
 
 -- [[ Auto Commands ]]
+-- Auto-quit nvim when NvimTree is last buffer
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
 
 -- [[ Key Bindings ]]
 -- Buffers
