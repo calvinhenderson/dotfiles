56462b9258e698907b818d476f45a125e6f87906
Calvin Henderson <henderson.calvin.w@gmail.com>
2024-02-12 10:06:12 -0500

    added more columns to layout grid

--- config/hammerspoon/plugins/window_grid.lua
+++ config/hammerspoon/plugins/window_grid.lua
@@ -1,18 +1,19 @@
 -- Configures a popup for grid-based window move/resize
 
-hs.grid.setGrid('6x3')
+hs.grid.setGrid('8x4')
+
 
 -- HINTS must contain at least 5 rows.
 -- Even if you are only using 3.. -\_(*_*)_/-
 hs.grid.HINTS = {
   -- unused
-  { 'f1', 'f2', 'f3', 'f4',  'f5',  'f6' },
-  { 'f7', 'f8', 'f9', 'f10', 'f11', 'f12' },
+  { 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8' },
 
   -- ISRT-based layout
-  { 'c',  'l',  'm',  'f',   'u',   ',' },
-  { 's',  'r',  't',  'n',   'e',   'a' },
-  { 'v',  'w',  'd',  'h',   '/',   '.' },
+  { 'c',  'l',  'm',  'k',  'f',  'z',  'u',  ',' },
+  { '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9' },
+  { 's',  'r',  't',  'g',  'n',  'p',  'e',  'a' },
+  { 'v',  'w',  'd',  'j',  'b',  'h',  '/',  '.' },
 }
 
 hs.grid.setMargins({ 0, 0 })
