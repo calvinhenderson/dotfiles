234c058ab456d204069aa960affb3edece4e83bf
Calvin Henderson <henderson.calvin.w@gmail.com>
2024-02-12 10:04:30 -0500

    migrated config to single config.lua

---
+++ config/hammerspoon/config.lua
@@ -0,0 +1,4 @@
+return {
+  leader = { mods = { "cmd", "ctrl", "alt", "shift" }, key = "h" },
+  config_root = os.getenv("HOME") .. "/.config/hammerspoon/"
+}
--- config/hammerspoon/util/paths.lua
+++
@@ -1,5 +0,0 @@
-local module = {}
-
-module.config_root = os.getenv("HOME") .. "/.config/hammerspoon/"
-
-return module
