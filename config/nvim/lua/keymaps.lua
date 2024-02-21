e18131f567e47a8d5d2122efa68f73ebb6394015
Calvin Henderson <henderson.calvin.w@gmail.com>
2024-02-21 17:08:29 -0500

    nvim: updated keymap

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -15,6 +15,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<Up>', "v:count == 0 ? 'gk' : '<Up>'", { expr = true, silent = true })
+vim.keymap.set('n', '<Down>', "v:count == 0 ? 'gj' : '<Down>'", { expr = true, silent = true })
 
 -- Buffers
 vim.keymap.set('n', '<leader>x', ':bp|bd #<CR>', { desc = 'E[x]it buffer, keep pane' })
